a41f4371f08a7e20f3432e6608a228cad133887c
hardik170220 <panchalhardik1702@gmail.com>
2025-10-31 18:20:00 +0530

    font err solve

---
+++ app/layout.tsx
@@ -0,0 +1,38 @@
+import type React from "react"
+import type { Metadata } from "next"
+// import { GeistSans } from "geist/font/sans"
+// import { GeistMono } from "geist/font/mono"
+import { Analytics } from "@vercel/analytics/next"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { Suspense } from "react"
+import { Providers } from "./providers"
+
+export const metadata: Metadata = {
+  title: "Ap-forms",
+  description: "Generate forms",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+                <link href="https://fonts.googleapis.com/css2?family=Anek+Gujarati:wght@100..800&family=Tiro+Devanagari+Hindi:ital@0;1&family=Yatra+One&display=swap" rel="stylesheet" />
+      </head>
+      <body className={`font-poppins`}>
+        <Suspense fallback={null}>
+          <Providers>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+            {children}
+            <Analytics />
+          </ThemeProvider>
+          </Providers>
+        </Suspense>
+      </body>
+    </html>
+  )
+}
